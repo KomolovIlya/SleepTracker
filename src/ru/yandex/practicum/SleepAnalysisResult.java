42f485d1a6893393af49b05c1f9c7cc0dd937f45
Ilya <komolov-ilyushka@yandex.ru>
2026-06-19 21:33:29 +0300

    Очистил master для пул-реквеста

--- src/ru/yandex/practicum/SleepAnalysisResult.java
+++
@@ -1,8 +0,0 @@
-package ru.yandex.practicum;
-
-public record SleepAnalysisResult<T>(String description, T value) {
-    @Override
-    public String toString() {
-        return description + ": " + value;
-    }
-}
